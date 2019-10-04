9120125b9adc36587fc46cff9770c8043876f7de
jsdev63 <react.dev@yahoo.com>
2019-10-04 03:32:29 -0700

    auth api call

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,20 +5,25 @@ import { setCurrentUser, registerSuccess, logoutUser, getErrors } from "./action
  * Login user action
  */
 export const loginUser = (state, history) => dispatch => {
-  AuthService.login('feyyaz@test.com', '1234').then(resp => {
-    if (resp.data.success) {
-      console.log(resp.data)
-      dispatch(setCurrentUser(resp.data.profile));
-      AuthService.saveToken(resp.data.token);
-      history.push('/');
-    }
-  }).catch(error => {
-    if (error.response.data) {
-      dispatch(getErrors({
-        loginError: error.response.data.error
-      }));
-    }
-  });
+  AuthService.auth().then(res => {
+    console.log('Authentication callback', res)
+  }).catch(err => {
+    console.log("err", err)
+  })
+  // AuthService.login('feyyaz@test.com', '1234').then(resp => {
+  //   if (resp.data.success) {
+  //     console.log(resp.data)
+  //     dispatch(setCurrentUser(resp.data.profile));
+  //     AuthService.saveToken(resp.data.token);
+  //     history.push('/');
+  //   }
+  // }).catch(error => {
+  //   if (error.response.data) {
+  //     dispatch(getErrors({
+  //       loginError: error.response.data.error
+  //     }));
+  //   }
+  // });
 }
 
 /**
