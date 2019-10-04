64ffe139ade49fbd6e0d22aebe5aa44e77e9e394
jsdev63 <react.dev@yahoo.com>
2019-10-04 03:41:58 -0700

    fixed cos error

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const config = {
         API_URL: "http://localhost:5000",
         APP_URL: "https://react-docusign.netlify.com",
         ClientID: "12386521-a32f-412e-9cea-0406255430f9",
-        RedirectURL: "https://react-docusign.netlify.com/callback"
+        RedirectURL: "https://react-docusign.netlify.com/ds/callback"
     },
     development: {
         API_URL: "http://localhost:5000"
