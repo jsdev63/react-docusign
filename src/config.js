b803765ecd278454b6e7b3d10b5117d820c0c146
jsdev63 <react.dev@yahoo.com>
2019-10-10 05:16:19 -0700

    test

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export const config = {
     // API_URL: "http://192.168.0.223:5000/docusign",
     clientID: '12386521-a32f-412e-9cea-0406255430f9',
     clientSecret: '093ebd1c-8d64-460e-8857-992334541bc1',
-    callbackURL: "https://react-docusign.jsdev63.now.sh/ds/callback",
+    callbackURL: "https://react-docusign.netlify.com/ds/callback",
     AuthURL: 'https://account-d.docusign.com/oauth'
 }
