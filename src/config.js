876c9d348cab76fb2cce01fcc70868487c0a8e8c
jsdev63 <react.dev@yahoo.com>
2019-10-10 04:28:39 -0700

    api endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    // API_URL: "https://real-server.herokuapp.com/docusign"
-    API_URL: "http://192.168.0.223:5000/docusign",
+    API_URL: "https://real-server.herokuapp.com/docusign",
+    // API_URL: "http://192.168.0.223:5000/docusign",
     clientID: '12386521-a32f-412e-9cea-0406255430f9',
     clientSecret: '093ebd1c-8d64-460e-8857-992334541bc1',
     callbackURL: "http://192.168.0.223:3000/ds/callback",
