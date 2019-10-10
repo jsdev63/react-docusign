43868253cb584dde0e4748153ef33bafbf95bd0b
jsdev63 <react.dev@yahoo.com>
2019-10-10 09:45:35 -0700

    updated prod env

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
+const callbackURL = process.env.NODE_ENV==='development'? "http://localhost:3000/ds/callback" : "https://react-docusign.jsdev63.now.sh/ds/callback"
+
 export const config = {
     API_URL: "https://real-server.herokuapp.com/docusign",
     // API_URL: "http://192.168.0.223:5000/docusign",
     clientID: '12386521-a32f-412e-9cea-0406255430f9',
     clientSecret: '093ebd1c-8d64-460e-8857-992334541bc1',
-    callbackURL: "https://react-docusign.jsdev63.now.sh/ds/callback",
+    callbackURL: callbackURL,
     AuthURL: 'https://account-d.docusign.com/oauth'
 }
 
